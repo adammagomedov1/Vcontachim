89d563f48361849798c1653c8e32e3f92ca234a4
Adam Magomedov <adam5770125@gmail.com>
2023-04-14 15:41:49 +0300

    добавил коментарий

--- app/src/main/java/com/example/vcontachim/MainActivity.kt
+++ app/src/main/java/com/example/vcontachim/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // при перемоздпние икрана выполняется этот код
         if (savedInstanceState == null) {
             goToSplashScreen()
         }
