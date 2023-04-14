7e76ba039e483712477a48b6c2334ac64bac02c8
Adam Magomedov <adam5770125@gmail.com>
2023-04-14 15:36:22 +0300

    Исправил переворот экрана

--- app/src/main/java/com/example/vcontachim/MainActivity.kt
+++ app/src/main/java/com/example/vcontachim/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.vcontachim
 
-import android.accessibilityservice.AccessibilityService.ScreenshotResult
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.vcontachim.fragment.VcontachimApplication
-import com.github.terrakok.cicerone.Screen
 import com.github.terrakok.cicerone.androidx.AppNavigator
-import java.util.Scanner
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -15,7 +11,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        goToSplashScreen()
+
+        if (savedInstanceState == null) {
+            goToSplashScreen()
+        }
     }
 
     private fun goToSplashScreen() {
