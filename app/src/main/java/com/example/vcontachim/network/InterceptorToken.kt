b541c17c992bba54425b9fd5e2938cf085f3af22
Adam Magomedov <adam5770125@gmail.com>
2023-05-31 15:11:29 +0300

    Убрал дублирование параметра v (version) в сетевых запросах

--- app/src/main/java/com/example/vcontachim/network/InterceptorToken.kt
+++ app/src/main/java/com/example/vcontachim/network/InterceptorToken.kt
@@ -5,11 +5,27 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
-class InterceptorToken: Interceptor {
+class InterceptorToken : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request: Request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer ${VcontachimApplication.sharedPreferencesHelper.tookToken}")
+        var request: Request = chain.request()
+            .newBuilder()
+            .addHeader(
+                "Authorization",
+                "Bearer ${VcontachimApplication.sharedPreferencesHelper.tookToken}"
+            )
             .build()
+
+        val url = request
+            .url()
+            .newBuilder()
+            .addQueryParameter("v", 5.131.toString())
+            .build()
+
+        request = request
+            .newBuilder()
+            .url(url)
+            .build()
+
         return chain.proceed(request)
     }
 }
--- app/src/main/java/com/example/vcontachim/network/VcontachimService.kt
+++ app/src/main/java/com/example/vcontachim/network/VcontachimService.kt
@@ -9,65 +9,55 @@ import retrofit2.http.Query
 interface VcontachimService {
     @GET("users.get")
     suspend fun getUsers(
-        @Query("v") v: Double = 5.131,
         @Query("fields") fields: String = "contacts,photo_200"
     ): Users
 
     @GET("friends.get")
     suspend fun getFriends(
-        @Query("v") v: Double = 5.131,
         @Query("fields") fields: String = "photo_200"
     ): Friends
 
     @GET("groups.get")
     suspend fun getGroups(
-        @Query("v") v: Double = 5.131,
         @Query("extended") extended: Int = 1,
         @Query("fields") fields: String = "members_count,verified",
     ): Communities
 
     @GET("photos.getAlbums")
     suspend fun getAlbumsPhotos(
-        @Query("v") v: Double = 5.131,
         @Query("need_covers") needCovers: Int = 1,
     ): PhotoAlbums
 
     @GET("photos.get")
     suspend fun getPhotos(
-        @Query("v") v: Double = 5.131,
         @Query("extended") extended: Int = 1,
         @Query("album_id") albumId: Long
     ): Photos
 
     @GET("video.get")
     suspend fun getVideo(
-        @Query("v") v: Double = 5.131,
         @Query("extended") extended: Int = 1
     ): Video
 
     @POST("likes.add")
     suspend fun addLike(
-        @Query("v") v: Double = 5.131,
         @Query("type") type: String = "photo",
         @Query("item_id") itemId: String
     ): Likes
 
     @POST("likes.delete")
     suspend fun deleteLike(
-        @Query("v") v: Double = 5.131,
         @Query("type") type: String = "photo",
         @Query("item_id") itemId: String
     ): Likes
 
     @POST("video.delete")
     suspend fun deleteVideo(
-        @Query("v") v: Double = 5.131,
         @Query("video_id") videoId: Long
     )
 
     @GET("likes.add")
     suspend fun addLikeVideo(
-        @Query("v") v: Double = 5.131,
         @Query("type") type: String = "video",
         @Query("item_id") itemId: Long,
         @Query("owner_id") ownerId: Long
@@ -75,7 +65,6 @@ interface VcontachimService {
 
     @GET("likes.delete")
     suspend fun deleteLikeVideo(
-        @Query("v") v: Double = 5.131,
         @Query("type") type: String = "video",
         @Query("item_id") itemId: Long,
         @Query("owner_id") ownerId: Long
@@ -83,7 +72,6 @@ interface VcontachimService {
 
     @GET("photos.getComments")
     suspend fun getCommentsPhotos(
-        @Query("v") v: Double = 5.131,
         @Query("photo_id") photoId: String,
         @Query("extended") extended: String = "profiles",
         @Query("fields") fields: String = "photo_200,online",
@@ -92,21 +80,18 @@ interface VcontachimService {
 
     @GET("likes.add")
     suspend fun addLikeComment(
-        @Query("v") v: Double = 5.131,
         @Query("type") type: String = "photo_comment",
         @Query("item_id") itemId: Int,
     )
 
     @GET("likes.delete")
     suspend fun deleteLikeComment(
-        @Query("v") v: Double = 5.131,
         @Query("type") type: String = "photo_comment",
         @Query("item_id") itemId: Int,
     )
 
     @POST("photos.createComment")
     suspend fun createCommentPhotos(
-        @Query("v") v: Double = 5.131,
         @Query("photo_id") photoId: String,
         @Query("message") message: String
     )
