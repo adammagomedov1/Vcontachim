e8aa26611a97f91e7751cc77ed5bcc87dc864e13
Adam Magomedov <adam5770125@gmail.com>
2023-07-14 17:50:55 +0300

    исправил недочёт

--- app/src/main/java/com/example/vcontachim/fragment/PeopleSearchFragment.kt
+++ app/src/main/java/com/example/vcontachim/fragment/PeopleSearchFragment.kt
@@ -63,29 +63,24 @@ class PeopleSearchFragment : Fragment(R.layout.fragment_people_search) {
             toast.show()
         }
 
-        Handler().postDelayed({
-            binding!!.editText.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
+        binding!!.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
 
-                }
+            }
 
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
-                }
+            }
 
-                override fun afterTextChanged(s: Editable?) {
-                    s?.filter { s.contains(s, ignoreCase = true) }
-                    viewModel.loadPeopleSearch(s.toString())
-
-                }
-            })
-        }, 1500)
-
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.loadPeopleSearch(s.toString())
+            }
+        })
 
         binding!!.imageViewDeleteIcon.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
