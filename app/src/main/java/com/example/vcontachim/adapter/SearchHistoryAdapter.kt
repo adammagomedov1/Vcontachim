4978970ec209a1d7398e535adefb8493a34d2c72
Adam Magomedov <adam5770125@gmail.com>
2023-08-11 12:53:09 +0300

    исправил недочёт

--- app/src/main/java/com/example/vcontachim/adapter/SearchHistoryAdapter.kt
+++ app/src/main/java/com/example/vcontachim/adapter/SearchHistoryAdapter.kt
@@ -44,7 +44,6 @@ class SearchHistoryAdapter(val clickListener: ClickListener) :
         }
     }
 
-
     object SearchHistoryDiffCallback : DiffUtil.ItemCallback<SearchHistory>() {
         override fun areItemsTheSame(
             oldItem: SearchHistory,
